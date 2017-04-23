af56e2177b2bdc684968a65755e5bcc2763e3775
Josef Vacek <vacekj@outlook.com>
2017-04-23 20:58:55 +0200

    new code yay

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,48 @@
 import * as $ from 'jquery';
+import * as request from 'request';
 
 class ServicioFrontend {
-    private readonly APIURL: String;
+    private readonly APIURL: string;
+    private readonly URL: {
+        User: "/user",
+        Authenticate: "/user/authenticate",
+        Register: "/user/register"
+
+    };
+    private Type: UserType;
     constructor(options: ServicioOptions) {
         this.APIURL = options.ApiUrl;
+        this.Type = options.Type;
     }
-    public getAPIKey(email: String, password: String): APIResponse {
-
+    public getAPIKey(email: string, password: string): APIResponse {
+        const options: request.CoreOptions = {
+            baseUrl: this.APIURL + this.URL[UserType[this.Type]] + this.URL.Authenticate
+        };
+        
         return new APIResponse(false, "Unknown error");
     }
 }
 
 class ServicioOptions {
-    ApiUrl: String;
-
+    public ApiUrl: string;
+    public Type: UserType;
+    constructor(apiUrl: string, userType: UserType) {
+        this.ApiUrl = apiUrl;
+        this.UserType = UserType;
+    }
 }
 
 class APIResponse {
-    public Message: String;
-    public Success: Boolean;
-    constructor(success: Boolean, message: String, attributes?: Object) {
+    public Message: string;
+    public Success: boolean;
+    constructor(success: boolean, message: string, attributes?: object) {
         this.Message = message;
         this.Success = success;
         Object.assign(this, attributes);
     }
+}
+
+enum UserType {
+    User,
+    Vendor
 }
